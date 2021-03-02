2d5fbfa4d15736189b3ef5d7687a7849f983c863
David Kalil Braga <dmmd10@hotmail.com>
2021-03-01 21:40:21 -0300

    First

---
+++ lib/AnimacaoImplicita.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimacaoImplicita extends StatefulWidget {
+  @override
+  _AnimacaoImplicitaState createState() => _AnimacaoImplicitaState();
+}
+
+class _AnimacaoImplicitaState extends State<AnimacaoImplicita> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
