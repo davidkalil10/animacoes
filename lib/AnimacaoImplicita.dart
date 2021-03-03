1a92498915057a0f0a5c7a8d3b5b5c21869358a4
David Kalil Braga <dmmd10@hotmail.com>
2021-03-02 21:53:06 -0300

    Animações básicas e Tween builder

--- lib/AnimacaoImplicita.dart
+++ lib/AnimacaoImplicita.dart
@@ -6,8 +6,34 @@ class AnimacaoImplicita extends StatefulWidget {
 }
 
 class _AnimacaoImplicitaState extends State<AnimacaoImplicita> {
+
+  bool _status = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          padding: EdgeInsets.all(20),
+          width: _status? 200 : 300,
+          height: _status? 300 : 200,
+          color: _status? Colors.white: Colors.purpleAccent,
+          child: Image.asset("imagens/logo.png"),
+          duration: Duration(seconds: 2),
+          //curve: Curves.easeInOutBack,
+          curve: Curves.elasticInOut,
+        ),
+        ElevatedButton(
+          child: Text("Alterar"),
+          onPressed: (){
+            setState(() {
+              _status = !_status;
+            });
+          },
+        )
+      ],
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:animacoes/AnimacaoImplicita.dart';
+import 'package:animacoes/AnimacaoTween.dart';
+import 'package:animacoes/CriandoAnimacoesBasicas.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: AnimacaoImplicita(),
+    home: AnimacaoTween(),
     debugShowCheckedModeBanner: false,
   ));
 }
